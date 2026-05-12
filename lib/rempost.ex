9c1f838863b6549b11ea0a150d95a9ac9de8fdeb
Remco <remcostoeten@hotmail.com>
2026-05-12 23:37:15 +0200

    Initialize Elixir application scaffold at repository root

---
+++ lib/rempost.ex
@@ -0,0 +1,18 @@
+defmodule Rempost do
+  @moduledoc """
+  Documentation for `Rempost`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rempost.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
